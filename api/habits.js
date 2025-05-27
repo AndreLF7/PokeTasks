0ef34418262bda3ddaef68425e2c005f0d85a39f
AndreLF7 <andrew.andryi@gmail.com>
2025-05-26 23:38:12 -0300

    Update habits.js

--- api/habits.js
+++ api/habits.js
@@ -1,1 +1,14 @@
 //hello world
+// api/habits.js
+import mongoose from 'mongoose';
+
+const uri = process.env.MONGODB_URI;
+
+const handler = async (req, res) => {
+  await mongoose.connect(uri);
+
+  // Exemplo: retornar um hábito fictício
+  res.status(200).json({ name: "Dormir cedo", done: false });
+};
+
+export default handler;
